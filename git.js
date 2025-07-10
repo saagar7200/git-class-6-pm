d5c079a3d86e763f7bf768f5f05007507b3bba45
Sagar Bhandari <rsaagar7200@gmail.com>
2025-07-10 18:46:26 +0545

    git commands

---
+++ git.js
@@ -0,0 +1,36 @@
+// git / git-hub
+
+// vcs -> 
+// colaboration between team members
+// to work on different features at same time (branching)
+
+// git repository
+
+// initialize  git repository
+// ? 1. git init
+
+// config
+//? git config --global --list -> list all git global config
+//?  git config --global  user.name <"user_name">
+
+// ? 2. git status
+// ? git add <file_path>
+// ? git add . -> stage all files
+// ? git commit -m <"commit_message">
+
+//! branch
+// ? git branch -> list all available branch
+//?  git branch <branch_name> -> create new branch
+//? git switch <branch_name> -> switch between branches
+
+//? git checkout <branch_name>
+//? git checkout -b <new_branch_name>
+
+// * Delete branch
+// ? git branch -D <branch_name>
+
+//! git hub / gitLab / bit bucket
+// ? list remote repo link
+//? git remote -v -> list remote origin
+//? git remote add origin https://github.com/saagar7200/git-class-6-pm.git  -> link with remote origin (git hub repo)
+// ? git remote remove origin  -> remove origin link
